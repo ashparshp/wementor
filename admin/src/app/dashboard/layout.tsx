650eedfc12a8365b78694dbef11c35fed6232934
Ashparshp <ashparsh.connects@gmail.com>
2026-07-05 02:06:16 +0530

    fix: add min-h-0 to dashboard layout and update logo image asset

--- admin/src/app/dashboard/layout.tsx
+++ admin/src/app/dashboard/layout.tsx
@@ -140,7 +140,7 @@ export default function DashboardLayout({
         </header>
 
         {/* Page Content */}
-        <main className="flex-1 overflow-y-auto p-4 lg:p-8">
+        <main className="flex-1 min-h-0 overflow-y-auto p-4 lg:p-8">
           <div className="max-w-7xl mx-auto">
             {children}
           </div>
