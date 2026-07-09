8e197f5bb91d112330b705aa59cc4538f5264ab7
Ashparshp <ashparsh.connects@gmail.com>
2026-07-09 14:52:50 +0530

    chore: move allowedDevOrigins out of experimental config in next.config.ts

--- student/next.config.ts
+++ student/next.config.ts
@@ -9,9 +9,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: ['soberly-subvermiform-wava.ngrok-free.dev'],
-  },
+  // @ts-ignore - Some NextConfig types haven't updated for this yet
+  allowedDevOrigins: ['soberly-subvermiform-wava.ngrok-free.dev'],
 };
 
 export default nextConfig;
